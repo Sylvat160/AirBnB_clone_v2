7a0e2e8fb36a841f6271b43e036edb94712d1214
Sylvain T <sylvaintagnabou@gmail.com>
2023-07-21 19:17:48 +0000

    06__07

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Starts a Flask web application
+    - The web application must be listening on
+    0.0.0.0 , port 5000
+    Routes:
+    - /states_list: display a HTML page: (inside the tag BODY)
+"""
+
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Displays a HTML page with a list of all States """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Closes the database again at the end of the request """
+    storage.close()
